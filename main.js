0385599cfa7906d29186e086332719cc3ebbbdfa
Shrirampro <101872044+Shrirampro@users.noreply.github.com>
2023-02-08 20:32:08 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/zBZGZ2gb/Clown-Nose-Download-PNG-Image.png');
 }
 
 function setup() {
@@ -6,12 +10,34 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded () {
+console.log('PoseNet Is Initialized');
 }
 
 function draw() {
     image(video, 0, 0, 640, 480);
+    fill(255,0,0);
+    stroke(255,0,0);
+    image(clown_nose, noseX-15, noseY-15, 30, 30);
 }
 
 function take_snapshot(){
     save('myFilterImage.png');
+}
+
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x =" + results[0].pose.nose.x)
+        console.log("nose y =" + results[0].pose.nose.y)
+    }
 }
